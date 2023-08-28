9f58484e268a1bf7612e37911f4daec66e7e6edb
starmorph <dylan@starmorph.com>
2023-08-28 15:02:33 -0700

    update URLs to scrape and index

--- src/app/components/Context/urls.ts
+++ src/app/components/Context/urls.ts
@@ -1,18 +1,20 @@
-export const urls = [{
+export const urls = [
+  {
+  url: "https://unitedtalent.com",
+  title: "United Talent",
+  seeded: false,
+  loading: false,
+},
+{
   url: "https://e360.yale.edu/digest/indonesia-malaysia-deforestation",
-  title: "Indonesia Deforestation",
+  title: "Variety",
   seeded: false,
   loading: false,
 },
+
 {
-  url: "https://cleantechnica.com/2023/06/29/solar-82-of-power-capacity-growth-in-india-in-2022/",
-  title: "Solar Power in India",
-  seeded: false,
-  loading: false,
-},
-{
-  url: "https://www.cbssports.com/nba/news/matisse-thybulle-to-stay-with-blazers-who-match-mavericks-three-year-33-million-deal-per-report/",
-  title: "Matisee Thybulle",
+  url: "https://www.utaspeakers.com/",
+  title: "UTA Speakers",
   seeded: false,
   loading: false,
 }]
