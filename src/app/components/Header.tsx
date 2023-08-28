d507d399ef9948c25ef7408ee92ba66ec59053b7
starmorph <dylan@starmorph.com>
2023-08-28 15:25:18 -0700

    update branding

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import PineconeLogo from "../../../public/pinecone.svg";
+import utadarktheme from "../../../public/utadarktheme.png";
 import VercelLogo from "../../../public/vercel.svg";
 
 export default function Header({ className }: { className?: string }) {
@@ -7,21 +7,23 @@ export default function Header({ className }: { className?: string }) {
     <header
       className={`flex items-center justify-center text-gray-200 text-2xl ${className}`}
     >
-      <Image
-        src={PineconeLogo}
+      <div className="text-4xl ml-3 mr-3 block text-center mx-auto">
+       <Image
+        src={utadarktheme}
         alt="pinecone-logo"
-        width="230"
-        height="50"
-        className="ml-3"
-      />{" "}
-      <div className="text-4xl ml-3 mr-3">+</div>
-      <Image
+        width="80"
+        height="64"
+        className="mx-auto my-auto bg-gray-800 text-center object-center "></Image>
+      <h1> Pinecone Starter</h1>
+      <p className="text-sm  text-gray-400">Scrape a website, vectorize the data, and ask questions with GPT-4</p>
+      </div>
+      {/* <Image
         src={VercelLogo}
         alt="vercel-logo"
         width="160"
         height="50"
         className="mr-3 mt-3"
-      />
+      /> */}
     </header>
   );
 }
